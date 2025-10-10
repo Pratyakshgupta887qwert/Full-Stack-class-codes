1b4673cf020bdea72c4a040049388a34fa1a928b
Pratyakshgupta887qwert <pratyakshgupta887@gmail.com>
2025-10-10 13:54:18 +0530

    updating 10-10-2025

--- 3.Javascript/js2.js
+++ 3.Javascript/js2.js
@@ -5,3 +5,40 @@ console.log(2<<1)
 // first convert 2 to 4 digit binary then shift towards right
 
 
+
+// condition and ternary operators
+let a = 2
+let b = 5
+if(a>b)
+    console.log("hello")
+else
+    console.log("bye")
+
+
+let a = 2;
+let b = 5;
+a > b ? console.log("hello") : console.log("bye");
+
+
+
+
+// nested
+let num1=1;
+let num2=13;
+let num3=8;
+if(num1>num2 && num1>num3){
+    console.log("num1 gr")
+}
+else if(num2>num3 && num2>num1){
+    console.log("num2 gr")
+}
+else if(num3>num2 && num3>num1){
+    console.log("num3 gr")
+}
+
+//switch
+switch (a>b && a>c){
+    case1 : console.log("a");
+    break;
+    case
+}
