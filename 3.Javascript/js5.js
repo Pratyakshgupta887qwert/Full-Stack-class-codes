a38302dad7b880988e3f9c85a96ff9fd7ba90128
Pratyakshgupta887qwert <pratyakshgupta887@gmail.com>
2025-10-16 15:08:22 +0530

    update

---
+++ 3.Javascript/js5.js
@@ -0,0 +1,9 @@
+for (let counter1 = 1; counter1 <= 10; counter1++) {
+    for (let counter2 = 1; counter2 <= 10; counter2++) {
+        console.log(`Row = ${counter1}, Column = ${counter2}`);
+
+        if (counter1 == 5) {
+
+        }
+    }
+}
