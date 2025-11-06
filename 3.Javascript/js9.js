fa3e15fe767dd0adf607614288932fc141ac0d7a
Pratyakshgupta887qwert <pratyakshgupta887@gmail.com>
2025-11-06 09:51:29 +0530

    06-11-2025

---
+++ 3.Javascript/js9.js
@@ -0,0 +1,6 @@
+// alert() : normal alert
+// prompt() : for input
+// confirm() : yes/no
+
+// pre defined diaogaol box : https://sweetalert2.github.io/#frameworks-integrations  or https://getbootstrap.com/docs/5.3/components/modal/
+
